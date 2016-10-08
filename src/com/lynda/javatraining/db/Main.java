6ed5ef1d3e8c8facb0826ff1253e84bbe9b5e978
Khairul <abmzk670@uowmail.edu.au>
2016-10-08 22:17:57 +0200

    Tours data extraction

--- src/com/lynda/javatraining/db/Main.java
+++ src/com/lynda/javatraining/db/Main.java
@@ -5,16 +5,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.lynda.javatraining.db.tables.Tours;
+
 public class Main {	
 	private static final String USERNAME = "dbuser";
 	private static final String PASSWORD = "dbpassword";	
 	public static void main(String args[]) throws SQLException {
 		try (Connection conn = DBUtil.getConnection(DBType.MYSQL);
 				Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-				ResultSet rs = stmt.executeQuery("SELECT stateId, stateName FROM states");
+//				ResultSet rs = stmt.executeQuery("SELECT stateId, stateName FROM states");
+				ResultSet rs = stmt.executeQuery("SELECT * FROM tours");
 				) {								
-			rs.last();
-			System.out.println("Number of rows: "+rs.getRow());
+//			rs.last();
+//			System.out.println("Number of rows: "+rs.getRow());
+			Tours.displayData(rs);
 		}catch(SQLException e){
 			DBUtil.processException(e);
 		}
