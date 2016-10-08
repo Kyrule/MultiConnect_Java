0f4707d3b648fcd125de61750e1becb3979550c8
Khairul <abmzk670@uowmail.edu.au>
2016-10-08 21:03:47 +0200

    Initial commit

---
+++ src/com/lynda/javatraining/db/Main.java
@@ -0,0 +1,22 @@
+package com.lynda.javatraining.db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {	
+	private static final String USERNAME = "dbuser";
+	private static final String PASSWORD = "dbpassword";	
+	public static void main(String args[]) throws SQLException {
+		try (Connection conn = DBUtil.getConnection(DBType.MYSQL);
+				Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+				ResultSet rs = stmt.executeQuery("SELECT * FROM states");
+				) {								
+			rs.last();
+			System.out.println("Number of rows: "+rs.getRow());
+		}catch(SQLException e){
+			System.err.println(e);
+		}
+	}
+}
