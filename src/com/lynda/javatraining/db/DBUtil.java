19d5c3a1fe6e0a257eb21a20ea0a09420c1d8ea7
Khairul <abmzk670@uowmail.edu.au>
2016-10-08 21:47:57 +0200

    With exception handling included into the program

--- src/com/lynda/javatraining/db/DBUtil.java
+++ src/com/lynda/javatraining/db/DBUtil.java
@@ -21,4 +21,10 @@ public class DBUtil {
 		}
 			
 	}
+	
+	public static void processException(SQLException e){
+		System.err.println("Error message: "+e.getMessage());
+		System.err.println("Error code: "+e.getErrorCode());
+		System.err.println("SQL state: "+e.getSQLState());
+	}
 }
--- src/com/lynda/javatraining/db/Main.java
+++ src/com/lynda/javatraining/db/Main.java
@@ -11,12 +11,12 @@ public class Main {
 	public static void main(String args[]) throws SQLException {
 		try (Connection conn = DBUtil.getConnection(DBType.MYSQL);
 				Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-				ResultSet rs = stmt.executeQuery("SELECT * FROM states");
+				ResultSet rs = stmt.executeQuery("SELECT stateId, stateName FROM states");
 				) {								
 			rs.last();
 			System.out.println("Number of rows: "+rs.getRow());
 		}catch(SQLException e){
-			System.err.println(e);
+			DBUtil.processException(e);
 		}
 	}
 }
